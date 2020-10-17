5c4cf15ef8ad33e279e37184537e7112b489d39b
NW\phe <peter@netwealth.com.au>
2020-10-17 22:14:15 +1100

    Install store dependency

--- car/apps/pricing/src/main.tsx
+++ car/apps/pricing/src/main.tsx
@@ -5,9 +5,25 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './containers/app';
 
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
